04d578414c3f1c7b19e6a0bb20b9b93ec89a5b0b
mark91 <mark.mekhayl@gmail.com>
2023-07-25 19:10:32 +1000

    changed contact to icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,15 @@ let Contact = () => {
     <div className="contact">
       <h1>Contact</h1>
       <div className='contact-list'>
-        <a href="mark.mekhayl@gmail.com">Email</a>
-        <a href="https://www.linkedin.com/in/mark-mekhayl-393834137/">Linkedin</a>
-        <a href="https://github.com/Lozlink">GitHub</a>
+        <a href="mark.mekhayl@gmail.com">
+          <img src="https://cdn-icons-png.flaticon.com/512/542/542638.png" alt="" />
+        </a>
+        <a href="https://www.linkedin.com/in/mark-mekhayl-393834137/">
+          <img src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="" />
+        </a>
+        <a href="https://github.com/Lozlink">
+          <img src="https://i.imgur.com/AQcYmsVt.png" alt="" />
+        </a>
       </div>
     </div>
   )
